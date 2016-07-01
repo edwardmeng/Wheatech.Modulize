47198e986d71b21e382a9711f94e2963392292ee
孟德华 <edwardmeng@live.cn>
2016-07-01 23:15:11 +0800

    Refactor location strategies.

---
+++ src/Location/ModuleLocatorCollection.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Wheatech.Modulize.Properties;
+
+namespace Wheatech.Modulize
+{
+    public class ModuleLocatorCollection:ICollection<IModuleLocator>
+    {
+        private readonly List<IModuleLocator> _locators;
+
+        #region Constructors
+
+        public ModuleLocatorCollection()
+        {
+            _locators = new List<IModuleLocator>();
+        }
+
+        public ModuleLocatorCollection(IEnumerable<IModuleLocator> locators)
+        {
+            _locators = new List<IModuleLocator>(locators);
+        }
+
+        public ModuleLocatorCollection(int capacity)
+        {
+            _locators = new List<IModuleLocator>(capacity);
+        }
+
+        public ModuleLocatorCollection(params IModuleLocator[] locators)
+            : this((IEnumerable<IModuleLocator>)locators)
+        {
+        }
+
+        #endregion
+
+        public IEnumerator<IModuleLocator> GetEnumerator()
+        {
+            return _locators.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        internal void SetReadOnly(bool isReadOnly)
+        {
+            IsReadOnly = isReadOnly;
+        }
+
+        private void ValidateReadOnly()
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException(Strings.Collection_ReadOnly);
+            }
+        }
+
+        public void Add(IModuleLocator item)
+        {
+            ValidateReadOnly();
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            _locators.Add(item);
+        }
+
+        public void Clear()
+        {
+            ValidateReadOnly();
+            _locators.Clear();
+        }
+
+        public bool Contains(IModuleLocator item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            return _locators.Contains(item);
+        }
+
+        public void CopyTo(IModuleLocator[] array, int arrayIndex)
+        {
+            _locators.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(IModuleLocator item)
+        {
+            ValidateReadOnly();
+            return _locators.Remove(item);
+        }
+
+        public int Count => _locators.Count;
+
+        public bool IsReadOnly { get; private set; }
+
+        public IModuleLocator this[int index]
+        {
+            get { return _locators[index]; }
+            set
+            {
+                ValidateReadOnly();
+                _locators[index] = value;
+            }
+        }
+    }
+}
---
+++ src/Location/StaticModuleLocator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Wheatech.Modulize
+{
+    public class StaticModuleLocator : IModuleLocator
+    {
+        private readonly string _moduleType;
+        private readonly string _path;
+        private readonly DiscoverStrategy _strategy;
+
+        public StaticModuleLocator(string moduleType, string path, DiscoverStrategy strategy)
+        {
+            _moduleType = moduleType;
+            _path = path;
+            _strategy = strategy;
+        }
+
+        public IEnumerable<ModuleLocation> GetLocations()
+        {
+            yield return new ModuleLocation
+            {
+                ModuleType = _moduleType,
+                Location = PathUtils.ResolvePath(_path),
+                DiscoverStrategy = _strategy
+            };
+        }
+    }
+}
